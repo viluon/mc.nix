f43ca8a96c395864f4818da86933f50ab9f0f7e1
Andrew Kvapil <git@viluon.me>
2023-09-25 17:57:22 +0200

    Fix modlist

--- flake.nix
+++ flake.nix
@@ -11,21 +11,19 @@
   outputs = { self, nixpkgs, minecraft, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-      inherit (pkgs) fetchurl;
-    in {
-      packages.mc-tech-scot =
-        (minecraft.legacyPackages.${system}.v1_19_4.fabric.client.withConfig [{
+      modded = (side:
+        (minecraft.legacyPackages.${system}.v1_20_1.fabric.${side}.withConfig [{
           mods = [
             (fetchurl {
               # file name must have a ".jar" suffix to be loaded by fabric
               name = "fabric-api.jar";
-              url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/uIYkhRbX/fabric-api-0.86.1%2B1.19.4.jar";
-              hash = "sha256-Ztz1c03gBM4twnM7mT14MbbfshR2cstFPozQxlhzL/Y=";
+              url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/1sf8i9fy/fabric-api-0.89.0%2B1.20.1.jar";
+              hash = "sha256-phvkSyGN6Yr1jq07qGcsC22ZG5WkQCCLUh5uW9pFsOc=";
             })
             (fetchurl {
               # CC:Tweaked
-              url = "https://cdn.modrinth.com/data/gu7yAYhd/versions/mtL4uUWR/cc-tweaked-1.19.4-fabric-1.108.0.jar";
-              hash = "sha256-3VGx5C3jEv/H9oGKA7b2++g44MhEefPZuUCWqjw9/tA=";
+              url = "https://cdn.modrinth.com/data/gu7yAYhd/versions/s6Ov3R0y/cc-tweaked-1.20.1-fabric-1.108.1.jar";
+              hash = "sha256-mWGdPIcuDBT4p88UPPxhO+9Ha759oBJnj9B65JecRk0=";
             })
             # (fetchurl {
             #   # Create
@@ -54,19 +52,29 @@
             # })
             (fetchurl {
               # Moonlight Lib
-              url = "https://cdn.modrinth.com/data/twkfQtEc/versions/gmoV0tyd/moonlight-1.19.4-2.4.16-fabric.jar";
-              hash = "sha256-qBuNfmnva1iFl8Fq9V+/l58W9eklUaSDQEjV2PEl2IM=";
+              url = "https://cdn.modrinth.com/data/twkfQtEc/versions/pglf5GDl/moonlight-1.20-2.8.18-fabric.jar";
+              hash = "sha256-A9a6BHS+XeAQTbrOT+8oIdGITKLOUleP24PKGsexFl8=";
             })
             (fetchurl {
               # Supplementaries
-              url = "https://cdn.modrinth.com/data/fFEIiSDQ/versions/1mQDVX91/supplementaries-1.19.4-2.4.15-fabric.jar";
-              hash = "sha256-u5iZcG7I66eNUxHxVQFcciqpbj0xk/2dSDHaLEyFQVY=";
+              url = "https://cdn.modrinth.com/data/fFEIiSDQ/versions/UYfKyS1v/supplementaries-1.20-2.6.2-fabric.jar";
+              hash = "sha256-/d6jwp3Frk1jrEioKXqIzdCiFP1GUUSgxakuGHXeaSo=";
             })
           ];
           # withConfig is also composable
         }]).withConfig {
-          resourcePacks = [
-          ];
-        };
+        }
+      );
+      inherit (pkgs) fetchurl;
+    in {
+      packages.mc-tech-scot = builtins.listToAttrs (
+        builtins.map (
+          side: {
+            name = side;
+            value = modded side;
+          }
+        )
+        ["client" "server"]
+      );
     });
 }
